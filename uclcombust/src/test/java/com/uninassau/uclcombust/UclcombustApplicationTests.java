78e2cb9725951e557ffaadf9aa7179d26235a9ae
Dimas Carlos <dimascarlosss@hotmail.com>
2021-06-06 17:30:32 -0300

    Projeto criado

---
+++ uclcombust/src/test/java/com/uninassau/uclcombust/UclcombustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninassau.uclcombust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UclcombustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
